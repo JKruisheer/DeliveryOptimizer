4bb171b3f6016759a4fd2c65a1878be0d15646c8
Jesse Kruisheer <70973567+JKruisheer@users.noreply.github.com>
2023-05-29 19:16:27 +0200

    Added all the important starting files

---
+++ src/main/java/org/acme/PlanningResource.java
@@ -0,0 +1,13 @@
+package org.acme;
+
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+
+@Path("/api/v1/planning")
+public class PlanningResource {
+
+    @GET
+    public String planResource(){
+        return "Hello";
+    }
+}
